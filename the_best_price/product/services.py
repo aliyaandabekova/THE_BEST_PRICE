9dfe41fabd26ba58583b7b75af62ef984ae1ce43
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-24 18:15:20 +0300

    create function check_product_detail (product from db,check in api)

--- the_best_price/product/services.py
+++ the_best_price/product/services.py
@@ -4,10 +4,12 @@ from math import inf
 
 
 def get_product(product):
-    urls = ['http://192.168.2.133:8000/api/v1/eldorado/','http://192.168.2.133:8000/api/v1/magnit/','http://192.168.2.133:8000/api/v1/gucci/']
+    urls = {'eldorado':'http://192.168.2.133:8000/api/v1/eldorado/',
+            'magnit':'http://192.168.2.133:8000/api/v1/magnit/',
+            'gucci':'http://192.168.2.133:8000/api/v1/gucci/'}
     min_price = inf
     product_with_min_price = 0
-    for url in urls:
+    for shop,url in urls.items():
         response = requests.get(url).json()
         for p in response:
             price = p['price']
@@ -15,7 +17,26 @@ def get_product(product):
             if product in name and price < min_price:
                 min_price = price
                 product_with_min_price = p
+                product_with_min_price['shop'] = shop
     return product_with_min_price
+
+
+def check_product_detail(result):
+    urls = {'eldorado': 'http://192.168.2.133:8000/api/v1/eldorado/',
+            'magnit': 'http://192.168.2.133:8000/api/v1/magnit/',
+            'gucci': 'http://192.168.2.133:8000/api/v1/gucci/'}
+    for shop,url in urls.items():
+        response = requests.get(url).json()
+        for p in response:
+            if result.name in p['name'] and p['price'] < result.price:
+                result.price = p['price']
+                result.shop = shop
+                result.save()
+    return result
+
+
+
+
 
 
 
