48a9ad13049f788a391acecd87e58ed4641d9b88
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-24 18:15:47 +0300

    register apps: userprofile,order

--- the_best_price/the_best_price/settings.py
+++ the_best_price/the_best_price/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
-    'rest_framework'
+    'rest_framework',
+    'userprofile',
+    'order',
+
 ]
 
 MIDDLEWARE = [
