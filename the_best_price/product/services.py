875c3dc233b0f636e77a7775822299ac7fa1948b
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-25 18:25:19 +0300

    optimize check_product_detail, get_product(dict_of_urls -> new model-Shop)

--- the_best_price/product/services.py
+++ the_best_price/product/services.py
@@ -3,13 +3,16 @@ import requests
 from math import inf
 
 
-def get_product(product):
-    urls = {'eldorado':'http://192.168.2.133:8000/api/v1/eldorado/',
-            'magnit':'http://192.168.2.133:8000/api/v1/magnit/',
-            'gucci':'http://192.168.2.133:8000/api/v1/gucci/'}
+def get_product(shops,product):
+    # urls = {'eldorado':'http://192.168.2.133:8000/api/v1/eldorado/',
+    #         'magnit':'http://192.168.2.133:8000/api/v1/magnit/',
+    #         'gucci':'http://192.168.2.133:8000/api/v1/gucci/'}
     min_price = inf
     product_with_min_price = 0
-    for shop,url in urls.items():
+    print(shops)
+    for shop in shops:
+
+        url = shop.url
         response = requests.get(url).json()
         for p in response:
             price = p['price']
@@ -17,20 +20,18 @@ def get_product(product):
             if product in name and price < min_price:
                 min_price = price
                 product_with_min_price = p
-                product_with_min_price['shop'] = shop
+                product_with_min_price['shop'] = shop.name
     return product_with_min_price
 
 
-def check_product_detail(result):
-    urls = {'eldorado': 'http://192.168.2.133:8000/api/v1/eldorado/',
-            'magnit': 'http://192.168.2.133:8000/api/v1/magnit/',
-            'gucci': 'http://192.168.2.133:8000/api/v1/gucci/'}
-    for shop,url in urls.items():
+def check_product_detail(shops,result):
+    for shop in shops:
+        url = shop.url
         response = requests.get(url).json()
         for p in response:
             if result.name in p['name'] and p['price'] < result.price:
                 result.price = p['price']
-                result.shop = shop
+                result.shop = shop.name
                 result.save()
     return result
 
