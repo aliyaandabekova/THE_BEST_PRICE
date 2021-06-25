048937f1cdd9f1a24585f2e0d91f9463743c4849
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-25 18:21:52 +0300

    register model-Score

--- the_best_price/product/admin.py
+++ the_best_price/product/admin.py
@@ -7,3 +7,5 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['name','price','date_created','shop','score_from_api','score_of_our_users']
     readonly_fields = ['date_created','score_from_api','score_of_our_users']
 admin.site.register(Product,ProductAdmin)
+
+admin.site.register(Score)
