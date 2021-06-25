6d472d6cc86b1b0cbaf359c85b7f011110745434
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-25 18:31:26 +0300

    name for score

--- the_best_price/userprofile/urls.py
+++ the_best_price/userprofile/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register/',RegisterView.as_view()),
     path('profile/',ProfileView.as_view()),
     path('login/',LoginView.as_view()),
-    path('score/<int:product_id>/',ScoreView.as_view()),
+    path('score/<int:product_id>/',ScoreView.as_view(),name='score'),
 
 ]
