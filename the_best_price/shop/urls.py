cecc4cab9e6746876bd5c1c1b2171bf1c8e946c5
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-28 17:36:58 +0300

    create url for: score to Shop, list of shops, comment to Shop

--- the_best_price/shop/urls.py
+++ the_best_price/shop/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('score/<str:shop_name>/', ShopScoreView.as_view(),name='shop_score'),
+    path('shops/',ShopsView.as_view()),
+    path('comment/<str:shop_name>/',CommentView.as_view()),
 
 ]
