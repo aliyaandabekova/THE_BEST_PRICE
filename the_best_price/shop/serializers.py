d571a603023ee88c19456f81f11ab2cc65909ee0
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-28 17:34:55 +0300

    create serializers: CommentSerializer, CommentGetSerializer, ShopSerializer

--- the_best_price/shop/serializers.py
+++ the_best_price/shop/serializers.py
@@ -1,4 +1,22 @@
 from rest_framework import serializers
+from .models import *
 
 class ScoreForShopSerializer(serializers.Serializer):
     score = serializers.FloatField()
+
+
+class CommentSerializer(serializers.Serializer):
+    text = serializers.CharField()
+
+
+class CommentGetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    comment_set = CommentSerializer(many=True)
+    class Meta:
+        model = Shop
+        fields = ['name','url','avg_rate','comment_set']
