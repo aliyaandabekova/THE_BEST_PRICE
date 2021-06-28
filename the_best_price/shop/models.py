5b61b3f53de1b8c6d2215e7514596e67c069e80b
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-28 17:34:07 +0300

    add parameters in Shop(amount_rate,count_rate,avg_rate), create class_model Comment

--- the_best_price/shop/models.py
+++ the_best_price/shop/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from userprofile.models import *
 
 class Shop(models.Model):
     name = models.CharField(max_length=30)
     url = models.CharField(max_length=100)
     rate = models.FloatField(default=0)
-    # def __str__(self):
-    #     return self.name
+    amount_rate = models.FloatField(default=0)
+    count_rate = models.PositiveIntegerField(default=0)
+    avg_rate = models.FloatField(default=0)
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=100)
+    shop = models.ForeignKey(Shop,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
