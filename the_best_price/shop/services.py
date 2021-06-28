131ef45a9177c77335e45607be49415a1b732fcd
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-28 17:35:34 +0300

    create function for count avg score Shop

--- the_best_price/shop/services.py
+++ the_best_price/shop/services.py
@@ -1,4 +1,15 @@
 def check_order(orders,shop):
     for order in orders:
+
+
         if order.product.shop == shop.name:
             return True
+
+
+
+def count_avg_score(shop, score):
+    shop.rate = score
+    shop.amount_rate += shop.rate
+    shop.count_rate += 1
+    shop.avg_rate = shop.amount_rate / shop.count_rate
+    shop.save()
