c71e556a50260e5013fe85e8ec985cc2f1be6199
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-30 14:49:05 +0300

    register Shop with list_display, register class-Comment

--- the_best_price/shop/admin.py
+++ the_best_price/shop/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Shop,Comment])
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name','url','rate','amount_rate','count_rate','avg_rate']
+    readonly_fields = ['rate','amount_rate','count_rate','avg_rate']
+
+admin.site.register(Shop,ShopAdmin)
+admin.site.register(Comment)
 
