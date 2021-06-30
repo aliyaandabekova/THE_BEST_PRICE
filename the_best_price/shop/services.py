aac2167677858529865a41f1753113f8a504de63
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-30 14:54:26 +0300

    add round_avg_rate

--- the_best_price/shop/services.py
+++ the_best_price/shop/services.py
@@ -12,4 +12,5 @@ def count_avg_score(shop, score):
     shop.amount_rate += shop.rate
     shop.count_rate += 1
     shop.avg_rate = shop.amount_rate / shop.count_rate
+    shop.avg_rate = round(shop.avg_rate,2)
     shop.save()
